b6aafd6b60ce4cb514b09f89639fd6ef2d22c7ec
alvarovola <117198859+alvarovola@users.noreply.github.com>
2023-03-05 12:49:26 +0100

    Create CursoJavaTema3.java

---
+++ .idea/CursoJavaTema3.java
@@ -0,0 +1,10 @@
+package OpenBootCampCourse;
+
+public class concatenarTexto {
+    public static void main(String[] args) {
+        String[] textos = {"Texto1", "Texto2", "Texto3", "Texto4", "Texto5", "Texto6", "Texto7"};
+        for (int i=0; i < textos.length; i++){
+            System.out.println(textos[i]);
+        }
+    }
+}
